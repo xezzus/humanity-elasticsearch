4710ffb893e6e0f7b167998117295b9c91002f1a
Andrey-xezzus <xezzus@gmail.com>
2016-02-25 12:24:19 +0300

    Developing

--- ElasticSearchRaw.php
+++ ElasticSearchRaw.php
@@ -10,14 +10,18 @@ class ElasticSearchRaw {
         self::$port = $port;
     }
 
-    public function index($index){
-        $index = explode('/',$index);        
-        foreach($index as $key=>$value){
-            $value = trim($value);
-            if(empty($value)) unset($index[$key]);
+    public function index($index=false){
+        if($index !== false){
+            $index = explode('/',$index);        
+            foreach($index as $key=>$value){
+                $value = trim($value);
+                if(empty($value)) unset($index[$key]);
+            }
+            $index = implode('/',$index);
+            return new ElasticSearchRequest('http://'.self::$host.':'.self::$port.'/'.$index);
+        } else {
+            return new ElasticSearchRequest('http://'.self::$host.':'.self::$port);
         }
-        $index = implode('/',$index);
-        return new ElasticSearchRequest('http://'.self::$host.':'.self::$port.'/'.$index);
     }
 }
 ?>
